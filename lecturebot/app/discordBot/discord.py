4bab99acb93f2ae63558d222322179133b4d7e3e
hieuminh65 <hieuminhnguyen@usf.edu>
2023-10-22 20:07:45 -0400

    final

--- lecturebot/app/discordBot/discord.py
+++ lecturebot/app/discordBot/discord.py
@@ -28,7 +28,18 @@ class DClient(discord.Client):
 
         if command == '/bot' or command == self.user.mention:
             bot_response = chat_response(prompt=user_message)
-            await message.channel.send(bot_response)
+            if len(bot_response) > 2000:
+                parts = bot_response.split('\n')
+                current_message = ""
+                for part in parts:
+                    if len(current_message) + len(part) + 1 > 2000:
+                        await message.channel.send(current_message)
+                        current_message = ""
+                    current_message += part + "\n"
+                if current_message:
+                    await message.channel.send(current_message)
+            else:
+                await message.channel.send(bot_response)
 
 intents = discord.Intents.default()
 intents.messages = True
